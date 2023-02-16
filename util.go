9a45a75e2b7ec98fb7094442bc2e7f6e179922c8
Drew Hintz <dhintz@squareup.com>
2023-02-15 22:13:32 -0600

    cache npmjs package lookups in a local file

--- util.go
+++ util.go
@@ -1,6 +1,11 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func inSlice(what rune, where []rune) bool {
 	for _, r := range where {
@@ -13,4 +18,32 @@ func inSlice(what rune, where []rune) bool {
 
 func countLeadingSpaces(line string) int {
 	return len(line) - len(strings.TrimLeft(line, " "))
+}
+
+// reads line-delimited contents of a file into a map of strings
+func readMap(path string) map[string]bool {
+	file, err := os.Open(path)
+	if err != nil {
+		return map[string]bool{}
+	}
+	defer file.Close()
+
+	avail := map[string]bool{}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		avail[scanner.Text()] = true
+	}
+	return avail
+}
+
+// writes a map of strings to a line-delimited file
+func writeMap(lines map[string]bool, path string) {
+	file, _ := os.Create(path)
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	for key, _ := range lines {
+		fmt.Fprintln(writer, key)
+	}
+	writer.Flush()
 }
